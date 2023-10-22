45b396f7ca3f02709cdabc32be00dc0770b6ef4d
alnah <alexis.nahan@gmail.com>
2023-10-21 21:37:57 -0300

    feat: add a function to get randomly the computer's choice

---
+++ rock_paper_scissors.js
@@ -0,0 +1,25 @@
+/**
+ * Returns randomly "Rock", "Paper" or "Scissors"
+ * This function makes the computer play
+ * @return {string} The computer's choice
+ */
+function getComputerChoice() {
+  // Math.random returns a floating-point number between 0 and 1, so we scale it
+  // to a range of 3, and then we round it up to obtain a number between 1 and 3
+  const randomNumber = Math.ceil((Math.random() * 3));
+  // init value for computerChoice is an empty string
+  let computerChoice = "";
+  // we assign "rock", "paper" or "scissors" thanks to our random number
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
+}
