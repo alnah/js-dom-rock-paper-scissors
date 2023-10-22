24dc0ffb072eaec739de19e87236617f04a2941a
alnah <alexis.nahan@gmail.com>
2023-10-22 07:30:40 -0300

    doc: use imperative form instead of conjugated form

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -6,16 +6,16 @@ their choice through a prompt
 
 
 /**
- * Generates a random number between 1 and a given maximum
+ * Generate a random number between 1 and a given maximum
  *
  * @param {number} number - The upper limit of the random number
- * @returns {number} A random number between 1 and `n` inclusive
+ * @returns {number} A random number between 1 and `number` inclusive
  */
 let getPositiveRandomNumberUpTo = (number) => Math.ceil((Math.random() * number));
 
 
 /**
- * Converts a number representation into one of the game choices: "rock",
+ * Convert a number representation into one of the game choices: "rock",
  * "paper", or "scissors"
  *
  * @param {number} number - A random number between 1 and 3
@@ -42,7 +42,7 @@ function getComputerChoice(number) {
 
 
 /**
- * Capitalizes the first letter of the given selection and makes the rest
+ * Capitalize the first letter of the given selection and makes the rest
  * lowercase
  *
  * @param {string} selection - A game choice string ("rock", "paper", or
@@ -55,7 +55,7 @@ let capitalizeSelection = (selection) => {
 
 
 /**
- * Compares the player's choice and the computer's choice to determine the
+ * Compare the player's choice and the computer's choice to determine the
  * winner of a round
  *
  * @param {string} playerSelection - The player's choice for the round
@@ -95,7 +95,7 @@ function playRound(playerSelection, computerSelection) {
 
 
 /**
- * Orchestrates multiple rounds of the game and determines the overall winner
+ * Orchestrate multiple rounds of the game and determines the overall winner
  *
  * @param {number} maxRounds - The total number of rounds to be played
  * @returns {number}
